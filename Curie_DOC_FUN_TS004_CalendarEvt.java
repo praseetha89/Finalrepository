a6a04304e92f57849f58d38b03034839f2ed8202
praseetha89 <praseetharathish89@gmail.com>
2017-11-09 16:39:02 +0530

    changes done

--- Curie_DOC_FUN_TS004_CalendarEvt.java
+++ Curie_DOC_FUN_TS004_CalendarEvt.java
@@ -1,10 +1,7 @@
 
 
-public class Curie_DOC_FUN_TS004_CalendarEvt extends OpenURL
-{
-  public Logger logger = Logger.getLogger(Curie_DOC_FUN_TS004_CalendarEvt.class.getName());
-	
-  @Test(dataProvider="dp")    
+
+   
   public void calendar(String username,String password,String date) throws Exception
   {
 	   
